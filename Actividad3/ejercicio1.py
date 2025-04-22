6a6de7deecd5cc66acd51d864a58866a5934cc09
Rommel110207 <rommelmg@uamv.edu.ni>
2025-04-22 07:31:48 -0600

    Calculo de promedio

---
+++ Actividad3/ejercicio1.py
@@ -0,0 +1,10 @@
+nota1= int(input("Ingrese la primera nota: "))
+nota2= int(input("Ingrese la segunda nota: "))
+nota3= int(input("Ingrese la tercera nota: "))
+suma= nota1 + nota2 + nota3
+promedio= suma / 3
+
+print(f"""calificacion 1: {nota1}
+calificacion 2: {nota2})
+calificacion 3: {nota3}
+promedio: {promedio:.0f}""")
