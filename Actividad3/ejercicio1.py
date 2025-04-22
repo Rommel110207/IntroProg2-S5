012e67b457857952e1a1fcdc4ae4235fe22d3ebe
Rommel110207 <rommelmg@uamv.edu.ni>
2025-04-22 07:47:46 -0600

    Se realizo un formato a la salida

--- Actividad3/ejercicio1.py
+++ Actividad3/ejercicio1.py
@@ -4,7 +4,7 @@ nota3= int(input("Ingrese la tercera nota: "))
 suma= nota1 + nota2 + nota3
 promedio= suma / 3
 
-print(f"""calificacion 1: {nota1}
-calificacion 2: {nota2})
-calificacion 3: {nota3}
-promedio: {promedio:.0f}""")
+print(f"""calificacion 1: {nota1:>3}
+calificacion 2: {nota2:>3}
+calificacion 3: {nota3:>3}
+{"promedio:":<15} {promedio:>3.0f}""")
