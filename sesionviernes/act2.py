0b33d901c6bfb8c7fc2e74d7c6693686f8621883
Rommel110207 <rommelmg@uamv.edu.ni>
2025-04-28 14:26:16 -0600

    Bonos

---
+++ sesionviernes/act2.py
@@ -0,0 +1,11 @@
+sueldo= float(input("Ingrese el sueldo: "))
+Bono= sueldo*0.10
+if sueldo >= 1500:
+    print(f"El bono es: {Bono}")
+elif sueldo >= 1000 and sueldo <= 1500:
+    print(f"El bono es: {Bono}")
+elif sueldo >= 500 and sueldo <= 1000:
+    print(f"El bono es: {Bono}")
+else: 
+    sueldo >= 0 and sueldo <= 500
+    print("no tiene bono")
