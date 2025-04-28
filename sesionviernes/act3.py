ff9458fe7c6f10500c6363fe98524921e63e058a
Rommel110207 <rommelmg@uamv.edu.ni>
2025-04-28 14:29:04 -0600

    Dias de la semana

---
+++ sesionviernes/act3.py
@@ -0,0 +1,23 @@
+print("Presiona 0 para salir")
+dia= int(input("Ingrese el dia de la semana (1-7)"))
+
+while dia != 0:
+    if dia == 1:
+        print("Lunes")
+    elif dia == 2:
+        print("Martes")
+    elif dia == 3:
+        print("Miercoles")
+    elif dia == 4:
+        print("Jueves")
+    elif dia == 5:
+        print("Viernes")
+    elif dia == 6:
+        print("Sabado")
+    elif dia == 7:
+        print("Domingo")
+    else:
+        print("Dia no valido")
+    print("Presiona 0 para salir")
+    dia = int(input("Ingrese el dia de la semana (1-7)")) 
+    
