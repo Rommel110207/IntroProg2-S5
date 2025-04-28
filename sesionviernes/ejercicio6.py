2a246db344e0321cd8c5c293e496fe771e678c56
Rommel110207 <rommelmg@uamv.edu.ni>
2025-04-28 14:43:18 -0600

    Calificacion

---
+++ sesionviernes/ejercicio6.py
@@ -0,0 +1,10 @@
+#Calificacion cualitativa
+calificacion = int(input("Ingrese la calificacion: "))
+if calificacion >=0 and calificacion <=70:
+    print("Reprobado")
+elif calificacion >70 and calificacion <=80:
+    print("Regular")
+elif calificacion >80 and calificacion <=90:
+    print("Bien")
+elif calificacion >90 and calificacion <=100:
+    print("Excelente")        
